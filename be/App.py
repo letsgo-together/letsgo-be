36f06f57d3b5efabfc8a9b5ddadedc4bade823d3
송준섭 <wnstjq7979@gmail.com>
2024-08-21 20:05:51 +0900

    remove: Room관련 계층 삭제

--- be/App.py
+++ be/App.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from router.ItemRouter import itemRouter
-from router.RoomRouter import roomRouter
 import sys
 from dotenv import load_dotenv
 
@@ -9,7 +8,6 @@ load_dotenv()
 app = Flask(__name__)
 
 app.register_blueprint(itemRouter)
-app.register_blueprint(roomRouter)
 
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
--- be/entity/RoomEntity.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-from typing import Optional
-
-
-@dataclass
-class RoomEntity:
-    id: Optional[int]
